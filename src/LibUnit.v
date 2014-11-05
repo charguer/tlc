56b90f8293b4f8536e750935a2d6617b6aaefcb4
charguer <arthur@chargueraud.org>
2014-11-05 11:27:46 +0100

    init

---
+++ src/LibUnit.v
@@ -0,0 +1,34 @@
+(**************************************************************************
+* Product of Data Structures                                              *
+**************************************************************************)
+
+Set Implicit Arguments.
+Require Import LibTactics LibLogic LibReflect.
+
+
+(* ********************************************************************** *)
+(** * Inhabited  *)
+
+Instance unit_inhab : Inhab unit.
+Proof. intros. apply (prove_Inhab tt). Qed.
+
+
+(* ********************************************************************** *)
+(** * Comparable *)
+
+Global Instance unit_comparable : Comparable unit.
+Proof.
+  apply make_comparable. intros [x]. destruct x.
+  rewrite* prop_eq_True_back. typeclass.
+Qed.
+
+
+(* ********************************************************************** *)
+(** * Structure *)
+
+Lemma unit_unique : forall tt1 tt2 : unit,
+  tt1 = tt2.
+Proof. intros. destruct tt1. destruct~ tt2. Qed.
+
+
+
