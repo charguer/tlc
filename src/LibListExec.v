eaff2ac279a1f47b1f8dfb5a7ed72d50882b7167
charguer <arthur@chargueraud.org>
2020-03-10 18:09:19 +0100

    fix lib list exec

--- src/LibListExec.v
+++ src/LibListExec.v
@@ -10,6 +10,22 @@ From TLC Require Export LibList.
 
 
 (* ---------------------------------------------------------------------- *)
+(* ** Autorewrite *)
+
+Module RewListExec.
+
+Tactic Notation "rew_list_exec" :=
+  autorewrite with rew_list_exec.
+Tactic Notation "rew_list_exec" "in" "*" :=
+  autorewrite_in_star_patch ltac:(fun tt => autorewrite with rew_list_exec).
+  (* autorewrite with rew_list_exec in *. *)
+Tactic Notation "rew_list_exec" "in" hyp(H) :=
+  autorewrite with rew_list_exec in H.
+
+End RewListExec.
+
+
+(* ---------------------------------------------------------------------- *)
 (* ** [is_nil] and [is_not_nil] *)
 
 Definition is_nil A (l:list A) : bool :=
@@ -34,6 +50,8 @@ Proof.
   intros. destruct l; simpl; rew_bool_eq; auto_false.
 Qed.
 
+Hint Rewrite is_nil_eq is_not_nil_eq : rew_list_exec.
+
 
 (* ---------------------------------------------------------------------- *)
 (* ** [length] *)
@@ -45,6 +63,8 @@ Lemma length_eq :
   length = LibList.length.
 Proof using. extens ;=> A l. induction l; simpl; rew_list; auto. Qed.
 
+Hint Rewrite length_eq : rew_list_exec.
+
 
 (* ---------------------------------------------------------------------- *)
 (* ** [app] *)
@@ -53,10 +73,12 @@ Definition app : forall A, list A -> list A -> list A :=
   List.app.
 
 Lemma app_eq :
-  List.app = LibList.app.
+  app = LibList.app.
 Proof using.
   extens ;=> A L1 L2. induction L1; simpl; rew_list; congruence.
 Qed.
+
+Hint Rewrite app_eq : rew_list_exec.
 
 
 (* ---------------------------------------------------------------------- *)
@@ -65,12 +87,14 @@ Qed.
 Definition rev : forall A, list A -> list A :=
   List.rev.
 
-Lemma List_rev_eq : forall A, (* --LATER: why fails if A is not quantified here? *)
+Lemma rev_eq : forall A, (* --LATER: why fails if A is not quantified here? *)
   @List.rev A = @LibList.rev A.
 Proof using.
   extens ;=> L. induction L; simpl; rew_list. { auto. }
-  { rewrite app_eq. simpl. congruence. }
+  { rewrite IHL. rewrite <- app_eq. unfold app. fequals. }
 Qed.
+
+Hint Rewrite rev_eq : rew_list_exec.
 
 
 (* ---------------------------------------------------------------------- *)
@@ -79,9 +103,11 @@ Qed.
 Definition fold_right : forall A B, (B->A->A) -> A -> list B -> A :=
   List.fold_right.
 
-Lemma List_fold_right_eq : forall A B (f:B->A->A) (a:A) (l:list B),
+Lemma fold_right_eq : forall A B (f:B->A->A) (a:A) (l:list B),
   fold_right f a l = LibList.fold_right f a l.
 Proof using. intros. induction l; simpl; rew_listx; fequals. Qed.
+
+Hint Rewrite fold_right_eq : rew_list_exec.
 
 
 (* ---------------------------------------------------------------------- *)
@@ -90,11 +116,13 @@ Proof using. intros. induction l; simpl; rew_listx; fequals. Qed.
 Definition map : forall A B, (A->B) -> list A -> list B :=
   List.map.
 
-Lemma List_map_eq :
-  List.map = LibList.map.
+Lemma map_eq :
+  map = LibList.map.
 Proof using.
   extens ;=> A B f L. induction L; simpl; rew_listx; congruence.
 Qed.
+
+Hint Rewrite map_eq : rew_list_exec.
 
 
 (* ---------------------------------------------------------------------- *)
@@ -112,6 +140,9 @@ Proof using. (* --LATER: conduct proof using list2_ind *)
   { auto. }
   { rew_list in E. rew_listx. simpl. fequals~. }
 Qed.
+
+Hint Rewrite combine_eq : rew_list_exec.
+
 
 (* ---------------------------------------------------------------------- *)
 (* ** [mem] *)
