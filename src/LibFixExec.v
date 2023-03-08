a200b21c76acc58ee18cb08e9411b5b6b9edb6a7
charguer <arthur@chargueraud.org>
2023-03-08 13:36:21 +0100

    qed fix exec

--- src/LibFixExec.v
+++ src/LibFixExec.v
@@ -192,15 +192,51 @@ Proof using. introv Hx. intros g. rewrite iter_swap. applys Hx. Qed.
 (* ********************************************************************** *)
 (** * Executing functions using variants in the non-termination monad *)
 
+(* ---------------------------------------------------------------------- *)
+(** ** Fixed point combinator in the non-termination monad *)
+
 (** [FixOpt] takes a functional expressed in the error monad, and returns
     its fixed point expressed in the non-termination monad. *)
 
-Definition FixOpt A B (Fopt:(A->option B)->(A->option B)) : nat->A->option B :=
-  fix fopt (n:nat) (x:A) : option B :=
+Definition FixOpt A B (G:(A->option B)->(A->option B)) : nat->A->option B :=
+  fix g (n:nat) (x:A) : option B :=
     match n with
     | O => None
-    | S n' => Fopt (fopt n') x
+    | S n' => G (g n') x
     end.
+
+(** A function built using [FixOpt] for a given [n] can only return proper (non-None)
+    results if [n > 0]. *)
+
+Lemma FixOpt_eq_Some_inv_pos : forall A B (G:(A->option B)->(A->option B)) n x,
+  FixOpt G n x <> None ->
+  (n > 0)%nat.
+Proof using. introv M. destruct n. { false* M. } { math. } Qed.
+
+
+(* ---------------------------------------------------------------------- *)
+(** ** Monotonicity property for functionals with respect to errors/timeouts *)
+
+Definition option_fun_incl A B (g1 g2:(A->option B)) : Prop :=
+  forall x y, g1 x = Some y -> g2 x = Some y.
+
+Definition error_monad_monotonic A B (G:(A->option B)->(A->option B)) : Prop :=
+  forall g1 g2, 
+  option_fun_incl g1 g2 ->
+  option_fun_incl (G g1) (G g2).
+
+Lemma FixOpt_mono_succ : forall A B (G:(A->option B)->(A->option B)) n x y, 
+  error_monad_monotonic G ->
+  FixOpt G n x = Some y ->
+  FixOpt G (S n) x = Some y.
+Proof using.
+  introv HG EQ. gen x y. induction n; introv EQ. 
+  { false. }
+  { simpls. applys HG EQ. intros g1 g2. applys IHn. }
+Qed.
+
+(* ---------------------------------------------------------------------- *)
+(** ** Relationship between a function and its monadic reformulation *)
 
 (** [is_monadic_variant f g] asserts that [g] is a reformulation of the function [f]
     in the error monad (with respect to correctness only, not w.r.t. to completeness). *)
@@ -231,6 +267,9 @@ Proof using.
   { simpls. applys HF EQ. intros a b Hab. applys* IHn. }
 Qed.
 
+(* ---------------------------------------------------------------------- *)
+(** ** Termination property and call-dependencies in the non-termination monad *)
+
 (** [terminates G x] asserts that the function [FixOpt Fopt] returns a
     proper output on the input [x], meaning that its execution does not 
     run out of fuel. *)
@@ -258,10 +297,97 @@ Lemma captures_dep_on_FixOpt : forall A B (G:(A->option B)->(A->option B)) (R : 
   fopt n y <> None.
 Proof using. introv M HR HN. applys M HR HN. Qed.
 
-Lemma FixOpt_eq_Some_inv_pos : forall A B (G:(A->option B)->(A->option B)) n x,
+(* ---------------------------------------------------------------------- *)
+(** ** Fixed point theorems for terminating executions *)
+
+(** On the domain where the functional in the non-termination monad terminates in n steps,
+    the functional [iter n F g] is a fixed point of [F], for any continuation [g]. 
+    Morever, this fixed point is consistent with any other fixed point of [F]. *)
+
+Lemma generally_consistent_partial_fixed_point_on_terminates : forall A B (F:(A->B)->(A->B)) G (n:nat),
+  is_ho_monadic_variant F G ->
+  error_monad_monotonic G ->
+  let D x := FixOpt G n x <> None in
+  forall g, generally_consistent_partial_fixed_point eq F (Build_partial (iter n F g) D).
+Proof using.
+  introv HFG HG. intros D g. split.
+  { intros h Hh. simpls. unfolds pfun_equiv.
+    intros x Dx. case_eq (FixOpt G n x); [|auto_false]. intros y Hxy.
+    applys eq_trans y.
+    { rewrite~ <- Hh. applys iter_of_is_ho_monadic_variant HFG Hxy. }
+    { lets M: HFG h (FixOpt G n) x y.
+      { clears x y. intros x y Hxy. rewrite <- Hh; [|auto_false]. 
+        applys iter_of_is_ho_monadic_variant HFG Hxy. }
+    rewrite~ M. applys FixOpt_mono_succ HG Hxy. } }
+  { intros [h D'] Hh. 
+    intros x (Dx&D'x). unfolds pfun_equiv. simpls.
+    case_eq (FixOpt G n x); [|auto_false]. intros y Hxy.
+    applys eq_trans y. { applys iter_of_is_ho_monadic_variant HFG Hxy. }
+    symmetry. gen x y. gen n. intros n. induction_wf IH: lt_wf n. intros.
+    { set (h' := fun x => If D' x then h x else iter n F g x).
+      specializes Hh h' __.
+      { clears x y. unfolds pfun_equiv. simpls. intros x D'x. unfold h'. case_if; auto. }
+      lets~ Hhx: Hh x __. unfold h' at 1 in Hhx. case_if. rewrites (rm Hhx ).
+      lets Hn: FixOpt_eq_Some_inv_pos Dx. destruct n as [|n']; [false;math|].
+      lets M: HFG h' (FixOpt G n') x y.
+      { clears x y. intros x y Hxy. unfold h'. case_if.
+        { applys IH n'; auto_false. }
+        { lets Hxy': FixOpt_mono_succ HG Hxy.
+          applys iter_of_is_ho_monadic_variant HFG Hxy'. } }
+      { rewrite~ M. } } }
+Qed.
+
+(* TODO: move *)
+Lemma FixFunMod_inv_at :
+  forall A (P:A->Prop) B {IB:Inhab B} (F:(A->B)->(A->B)) (E:binary B) (f f':A->B) (x:A),
+  f = FixFunMod E F ->
+  equiv E ->
+  generally_consistent_partial_fixed_point E F (Build_partial f' P) ->
+  P x ->
+  E (f x) (f' x).
+Proof using.
+  introv Deff Equiv Gcf' Px. lets EQ: FixFunMod_inv Deff Equiv Gcf'.
+  applys* equiv_sym Equiv.
+Qed.
+
+Lemma FixFun_eq_iter_on_terminates : forall A B {IB:Inhab B} f (F:(A->B)->(A->B)) G (n:nat) (x:A),
+  f = FixFun F ->
+  is_ho_monadic_variant F G ->
+  error_monad_monotonic G ->
   FixOpt G n x <> None ->
-  (n > 0)%nat.
-Proof using. introv M. destruct n. { false* M. } { math. } Qed.
+  forall g, f x = iter n F g x.
+Proof using.
+  introv Hf HFG HG Dx. intros g.
+  lets M: generally_consistent_partial_fixed_point_on_terminates HFG HG g.
+  applys* FixFunMod_inv_at M. { applys equiv_eq. }
+Qed.
+
+Lemma FixFun_eq_FixOpt : forall A B {IB:Inhab B} f (F:(A->B)->(A->B)) G (n:nat) (x:A) (y:B),
+  f = FixFun F ->
+  is_ho_monadic_variant F G ->
+  error_monad_monotonic G ->
+  FixOpt G n x = Some y ->
+  f x = y.
+Proof using.
+  introv Hf HFG HG Hxy. rewrites (>> FixFun_eq_iter_on_terminates n x Hf HFG HG f).
+  { auto_false. } { applys iter_of_is_ho_monadic_variant HFG Hxy. }
+Qed.
+
+Lemma partial_fixed_point_iter_on_terminates : forall A B (F:(A->B)->(A->B)) G (n:nat),
+  is_ho_monadic_variant F G ->
+  error_monad_monotonic G ->
+  let D x := FixOpt G n x <> None in
+  forall g, partial_fixed_point eq F (Build_partial (iter n F g) D).
+Proof using.
+  introv HFG HG. intros D g h Hh. simpls. unfolds pfun_equiv.
+  intros x Dx. case_eq (FixOpt G n x); [|auto_false]. intros y Hxy.
+  applys eq_trans y.
+  { rewrite~ <- Hh. applys iter_of_is_ho_monadic_variant HFG Hxy. }
+  { lets M: HFG h (FixOpt G n) x y.
+    { clears x y. intros x y Hxy. rewrite <- Hh; [|auto_false]. 
+      applys iter_of_is_ho_monadic_variant HFG Hxy. }
+  rewrite~ M. applys FixOpt_mono_succ HG Hxy. }
+Qed.
 
 (** The following fixed point induction principle asserts that, on the domain of input
     values [x] on which [FixFun Fopt] terminates with a proper output, one can reason
@@ -271,20 +397,19 @@ Lemma FixFun_fix_ter : forall A B {IB:Inhab B} f (F:(A->B)->(A->B)) (P:A->B->Pro
    (G:(A->option B)->(A->option B)) (R:A->A->Prop),
   f = FixFun F ->
   is_ho_monadic_variant F G ->
+  error_monad_monotonic G ->
   captures_dep G R ->
   (forall h x,
     (forall y, R y x -> P y (h y)) ->
     P x (F h x)) ->
   forall x, terminates G x -> P x (f x).
 Proof using.
-  introv Hf HG HR HI Hx. 
+  introv Hf HFG HG HR HI Hx. 
   destruct Hx as (n&Hx). case_eq (FixOpt G n x); [|auto_false]. intros y Hy.
-  lets MG: iter_of_is_ho_monadic_variant (rm HG).
+  lets MG: iter_of_is_ho_monadic_variant HFG.
   asserts HFnx: (FixFun_iter_indep F n x).
   { intros g1 g2. rewrites (>> MG g1 Hy). rewrites* (>> MG g2 Hy). }
-    (* alternative: { applys FixFun_iter_indep_of_image. intros g. rewrites* (>> MG g Hy). } *)
-  asserts Ef: (f x = iter n F f x). skip. (* TODO *)
-  rewrite Ef. clear Ef.
+  rewrites~ (>> FixFun_eq_iter_on_terminates x Hf HFG HG Hx f).
   lets Hn: FixOpt_eq_Some_inv_pos Hx.
   clear Hy HFnx y. gen Hn x. induction_wf IH: wf_lt n. intros Hn x Hx.
   destruct n as [| n']; [false;math|]. simpl. applys HI.
@@ -293,24 +418,11 @@ Proof using.
   applys IH Hy; try math.
 Qed.
 
+    (* alternative:
+       asserts HFnx: (FixFun_iter_indep F n x).
+       { applys FixFun_iter_indep_of_image. intros g. rewrites* (>> MG g Hy). } *)
 
-
-
-Lemma FixFun_fix_ter : forall A B {IB:Inhab B} f (F:(A->B)->(A->B)) (P:A->B->Prop)
-   (Fopt:(A->option B)->(A->option B)) (R:A->A->Prop),
-  f = FixFun F ->
-  is_ho_monadic_variant F Fopt ->
-  captures_dep Fopt R ->
-  (forall h x,
-    (forall y, R y x -> P y (h y)) ->
-    P x (F h x)) ->
-  forall x, terminates Fopt x -> P x (f x).
-Proof using.
-  forall g, iter n F g x = y.
-  FixFun_iter_indep F n x ->
-  forall g, f x = iter n F g x
-  then induction n
-Qed.
+(*
 
 Definition maximal_dep A B (f g : (A->option B)) : (A->A->Prop) :=
   fun x y => f x <> None -> g y <> None.
@@ -326,7 +438,7 @@ Proof using.
   def fdep x y := forall n, fopt (S n) x <> None -> fopt n y <> None.
   def hxSn := fun y => if fdep x y then fopt n y else g y.
 Qed.
-
+*)
 
 
 (* ********************************************************************** *)
@@ -350,7 +462,7 @@ Proof using.
   { intros g Hg x Dx. hnf in Hg.
     forwards K: IHn (Build_partial (iter n F g) (dom g)).
 
- skip. unfolds partial_fixed_point. unfolds fixed_point. hnf in K.
+ . unfolds partial_fixed_point. unfolds fixed_point. hnf in K.
 
 Qed.
 
@@ -450,7 +562,7 @@ Lemma FixFun_fix_iter_inv :
   (forall f', iter n F f' x = y) ->
   f x = y.
 Proof using.
- Admitted.
+
 
 
 
